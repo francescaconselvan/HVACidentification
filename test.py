92574e1da8c1878af939509e9ec91902762791db
LaFra <francescaconselvan@gmail.com>
2024-03-27 12:14:28 +0100

    test_HAVC_ident

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import pickle
+
+with open('HVAC_ident_model.pkl', 'rb') as f:
+    # Load the object from the pickle file
+    loaded_object = pickle.load(f)
+
+# Now you can use the loaded_object as you normally would
+print(loaded_object)
